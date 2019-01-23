56577801909b7a2f834ed0e7ca681f5d9d06106e
Igor Smirnov <igor99_99@inbox.ru>
2019-01-24 01:04:42 +0300

    implemented trie methods

--- 190123_trie/src/main/java/ru/spbhse/trie/Trie.java
+++ 190123_trie/src/main/java/ru/spbhse/trie/Trie.java
@@ -3,6 +3,7 @@ package ru.spbhse.trie;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 /**
  * Class realizing data structure Trie to store set of Unicode strings
@@ -10,18 +11,55 @@ import java.io.OutputStream;
  */
 public class Trie implements Serializable {
     private int size;
+    private HashMap<Character, Trie> nextNode;
+    private boolean isTerminal;
+
+    public Trie() {
+        nextNode = new HashMap<>();
+    }
 
     /**
      * Adds given element to trie
      * @return true if this element was not presented in a trie, false otherwise
      */
     public boolean add(String element) {
-        return true;
+        return addStartFrom(element, 0);
+    }
+
+    /**
+     * Recursive addition of element to trie
+     * @return true if this element was not presented in a trie, false otherwise
+     */
+    private boolean addStartFrom(String element, int charId) {
+        if (charId == element.length()) {
+
+            boolean isNew = !isTerminal;
+            if (isNew) {
+                ++size;
+                isTerminal = true;
+            }
+
+            return isNew;
+        }
+
+        char currentChar = element.charAt(charId);
+
+        if (!nextNode.containsKey(currentChar)) {
+            nextNode.put(currentChar, new Trie());
+        }
+
+        boolean isNew = nextNode.get(currentChar).addStartFrom(element, charId + 1);
+        if (isNew) {
+            ++size;
+        }
+
+        return isNew;
     }
 
     /** Returns true iff element is in trie */
     public boolean contains(String element) {
-        return false;
+        Trie prefixNode = goDownPrefix(element);
+        return prefixNode != null && prefixNode.isTerminal;
     }
 
     /**
@@ -29,17 +67,67 @@ public class Trie implements Serializable {
      * @return true iff given element was presented in a trie
      */
     public boolean remove(String element) {
-        return false;
+        return removeStartFrom(element, 0);
+    }
+
+    /**
+     * Recursive removing of element from trie
+     * @return true if this element was presented in a trie, false otherwise
+     */
+    private boolean removeStartFrom(String element, int charId) {
+        if (charId == element.length()) {
+
+            boolean wasInTrie = isTerminal;
+            if (wasInTrie) {
+                --size;
+                isTerminal = false;
+            }
+
+            return wasInTrie;
+        }
+
+        char currentChar = element.charAt(charId);
+
+        if (!nextNode.containsKey(currentChar)) {
+            return false;
+        }
+
+        Trie removingNode = nextNode.get(currentChar);
+        boolean wasInTrie = removingNode.removeStartFrom(element, charId + 1);
+        if (wasInTrie) {
+            --size;
+            if (removingNode.size == 0) {
+                nextNode.put(currentChar, null);
+            }
+        }
+
+        return wasInTrie;
     }
 
     /** Returns number of strings in a trie */
     public int size() {
-        return 0;
+        return size;
     }
 
     /** Returns number of strings in a trie that start with given prefix */
     public int howManyStartsWithPrefix(String prefix) {
-        return 0;
+        Trie prefixNode = goDownPrefix(prefix);
+        return prefixNode == null ? 0 : prefixNode.size;
+    }
+
+    /** Returns Node appropriated to given prefix (and null if it doesn't exist */
+    private Trie goDownPrefix(String prefix) {
+        Trie currentNode = this;
+        for (char c : prefix.toCharArray()) {
+
+            if (!currentNode.nextNode.containsKey(c)) {
+                return null;
+            }
+
+            currentNode = currentNode.nextNode.get(c);
+        }
+
+        return currentNode;
     }
 
     @Override
